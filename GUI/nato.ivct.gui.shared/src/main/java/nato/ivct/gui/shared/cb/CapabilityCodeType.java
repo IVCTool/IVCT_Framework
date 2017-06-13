5bf1a8d5b15af48a968e5adcf5282b5ad39f6fac
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2017-06-13 09:23:23 +0200

    to be used in SuT form

---
+++ GUI/nato.ivct.gui.shared/src/main/java/nato/ivct/gui/shared/cb/CapabilityCodeType.java
@@ -0,0 +1,35 @@
+package nato.ivct.gui.shared.cb;
+
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.rt.shared.services.common.code.AbstractCode;
+import org.eclipse.scout.rt.shared.services.common.code.AbstractCodeType;
+
+public class CapabilityCodeType extends AbstractCodeType<Integer, String> {
+
+	private static final long serialVersionUID = 1L;
+	public static final int ID = 0;
+
+	@Override
+	public Integer getId() {
+		return ID;
+	}
+
+	@Order(1000)
+	public static class CapCode extends AbstractCode<Integer> {
+		private static final long serialVersionUID = 1L;
+		public static final Integer ID = 0;
+
+		@Override
+		protected String getConfiguredText() {
+			return TEXTS.get("MyNlsKey");
+		}
+
+		@Override
+		public Integer getId() {
+			return ID;
+		}
+	}
+	
+	
+}
