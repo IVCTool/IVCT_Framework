2de5b168ccf7c7d3eb37ead3161a29e225df221b
Michael Theis <Michael.Theis@iosb.fraunhofer.de>
2018-12-17 17:05:35 +0100

    SuT edit form added (1st draft - t.b.c.)

---
+++ GUI/nato.ivct.gui.client/src/main/java/nato/ivct/gui/client/sut/SuTEditForm.java
@@ -0,0 +1,322 @@
+package nato.ivct.gui.client.sut;
+
+import org.eclipse.scout.rt.client.dto.FormData;
+import org.eclipse.scout.rt.client.ui.action.keystroke.IKeyStroke;
+import org.eclipse.scout.rt.client.ui.basic.table.AbstractTable;
+import org.eclipse.scout.rt.client.ui.basic.table.columns.AbstractColumn;
+import org.eclipse.scout.rt.client.ui.form.AbstractForm;
+import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
+import org.eclipse.scout.rt.client.ui.form.IForm;
+import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractButton;
+import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
+import org.eclipse.scout.rt.client.ui.form.fields.stringfield.AbstractStringField;
+import org.eclipse.scout.rt.client.ui.form.fields.tablefield.AbstractTableField;
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.platform.text.TEXTS;
+
+import nato.ivct.gui.client.sut.SuTEditForm.MainBox.SuTCapabilityBox;
+import nato.ivct.gui.client.sut.SuTEditForm.MainBox.GeneralBox;
+import nato.ivct.gui.client.sut.SuTEditForm.MainBox.GeneralBox.DescrField;
+import nato.ivct.gui.client.sut.SuTEditForm.MainBox.GeneralBox.NameField;
+import nato.ivct.gui.client.sut.SuTEditForm.MainBox.GeneralBox.SutVendorField;
+import nato.ivct.gui.shared.sut.CreateSuTPermission;
+import nato.ivct.gui.shared.sut.ISuTService;
+import nato.ivct.gui.shared.sut.SuTFormData;
+import nato.ivct.gui.shared.sut.UpdateSuTPermission;
+
+@FormData(value = SuTFormData.class, sdkCommand = FormData.SdkCommand.CREATE)
+public class SuTEditForm extends AbstractForm {
+
+	private String sutId = null;
+	private String title = null;
+
+	public SuTEditForm(String formTitle) {
+		title = formTitle;
+	}
+
+	@Override
+	protected String getConfiguredTitle() {
+		if (title != null)
+			return title;
+		else
+			return TEXTS.get("SuT");
+	}
+
+	public void startModify() {
+		startInternalExclusive(new ModifyHandler());
+	}
+
+	public void startNew() {
+		startInternal(new NewHandler());
+	}
+
+	public CloseButton getCloseButton() {
+		return getFieldByClass(CloseButton.class);
+	}
+
+//	public CancelButton getCancelButton() {
+//		return getFieldByClass(CancelButton.class);
+//	}
+//
+//	public OkButton getOkButton() {
+//		return getFieldByClass(OkButton.class);
+//	}
+
+	public MainBox getMainBox() {
+		return getFieldByClass(MainBox.class);
+	}
+
+	public GeneralBox getGeneralBox() {
+		return getFieldByClass(GeneralBox.class);
+	}
+
+	public DescrField getDescrField() {
+		return getFieldByClass(DescrField.class);
+	}
+
+	public SutVendorField getSutVendorField() {
+		return getFieldByClass(SutVendorField.class);
+	}
+
+	public SuTCapabilityBox getDetailsBox() {
+		return getFieldByClass(SuTCapabilityBox.class);
+	}
+
+	public NameField getNameField() {
+		return getFieldByClass(NameField.class);
+	}
+
+	@FormData
+	public String getSutId() {
+		return sutId;
+	}
+
+	@FormData
+	public void setSutId(final String _sutId) {
+		this.sutId = _sutId;
+	}
+
+	@Override
+	public Object computeExclusiveKey() {
+		return getSutId();
+	}
+
+	@Override
+	protected int getConfiguredDisplayHint() {
+		// TODO Auto-generated method stub
+		return IForm.DISPLAY_HINT_VIEW;
+	}
+
+	@Order(1000)
+	public class MainBox extends AbstractGroupBox {
+		
+        @Order(1000)
+        public class GeneralBox extends AbstractGroupBox {
+        	@Override
+        	protected String getConfiguredLabel() {
+        		return TEXTS.get("GeneralSuTInformation");
+        }
+        
+        // set all fields of this box to read-only
+        @Override
+        public boolean isEnabled() {
+        	return false;
+        }
+        
+        @Order(1100)
+        public class NameField extends AbstractStringField {
+        	@Override
+        	protected String getConfiguredLabel() {
+        		return TEXTS.get("SuTName");
+        	}
+        
+        	@Override
+        	protected int getConfiguredMaxLength() {
+        		return 128;
+        	}
+        }
+        
+        @Order(1200)
+        public class SutVendorField extends AbstractStringField {
+        	@Override
+        	protected String getConfiguredLabel() {
+        		return TEXTS.get("SuTVendor");
+        	}
+        
+        	@Override
+        	protected int getConfiguredMaxLength() {
+        		return 128;
+        	}
+        }
+        
+        @Order(1300)
+        public class DescrField extends AbstractStringField {
+        	@Override
+        	protected String getConfiguredLabel() {
+        		return TEXTS.get("SuTDescription");
+        	}
+        
+        	@Override
+        	protected int getConfiguredGridW() {
+        		return 3;
+        	}
+        
+        	@Override
+        	protected int getConfiguredGridH() {
+        		return 2;
+        	}
+        
+        	@Override
+        	protected int getConfiguredMaxLength() {
+        		return 256;
+        	}
+        	
+        	// set to multi-line
+        		@Override
+        		protected boolean getConfiguredMultilineText() {
+        			return true;
+        		}
+        	}
+        }
+
+		@Order(2000)
+		public class SuTCapabilityBox extends AbstractGroupBox {
+			
+			@Order(2100)
+			public class SutCapabilityTableField extends AbstractTableField<SutCapabilityTableField.SutCapabilityTable> {
+				@Override
+				protected int getConfiguredGridW() {
+					return 3;
+				}
+				
+				@Override
+				protected String getConfiguredLabel() {
+					return TEXTS.get("CapabilityStatus");
+				}
+
+				public class SutCapabilityTable extends AbstractTable {
+					@Order(2110)
+					public class CbBadgeID extends AbstractColumn<String> {
+						@Override
+						protected String getConfiguredHeaderText() {
+							return TEXTS.get("BadgeId");
+						}
+
+						@Override
+						protected int getConfiguredWidth() {
+							return 200;
+						}
+					}
+					
+					@Order(2120)
+					public class CbBadgeStatus extends AbstractColumn<String> {
+						@Override
+						protected String getConfiguredHeaderText() {
+							return TEXTS.get("BadgeConformanceStatus");
+						}
+
+						@Override
+						protected int getConfiguredWidth() {
+							return 100;
+						}
+					}
+				}
+			}
+		}
+
+	}
+		
+		
+		
+	@Order(100000)
+	public class CloseButton extends AbstractButton {
+		
+		public CloseButton() {
+			super();
+			// set button invisible by default
+			setVisible(false);
+		}
+		
+		@Override
+		protected int getConfiguredSystemType() {
+		    return SYSTEM_TYPE_CLOSE;
+		}
+		
+		@Override
+		protected String getConfiguredLabel() {
+		    return TEXTS.get("CloseButton");
+		}
+		
+		@Override
+		protected String getConfiguredKeyStroke() {
+		    return IKeyStroke.ESCAPE;
+		}
+		  
+		@Override
+		public boolean isVisible() {
+			// TODO Auto-generated method stub
+			return super.isVisible();
+		}
+	}
+
+//	@Order(101000)
+//	public class CancelButton extends AbstractCancelButton {
+//	}
+//
+//	@Order(100000)
+//	public class OkButton extends AbstractOkButton {
+//	}
+
+	protected abstract class AbstractSuTFormHandler extends AbstractFormHandler {
+		@Override
+		protected void execLoad() {
+			ISuTService service = BEANS.get(ISuTService.class);
+			SuTFormData formData = new SuTFormData();
+			exportFormData(formData);
+			formData = service.load(formData);
+			importFormData(formData);
+//			getForm().setSubTitle(formData.getName().getValue());
+
+			setEnabledPermission(new UpdateSuTPermission());
+		}
+	}
+	
+	public class ModifyHandler extends AbstractSuTFormHandler {
+
+		@Override
+		protected void execLoad() {
+			super.execLoad();
+		}
+
+		@Override
+		protected void execStore() {
+			ISuTService service = BEANS.get(ISuTService.class);
+			SuTFormData formData = new SuTFormData();
+			exportFormData(formData);
+			service.store(formData);
+		}
+	}
+
+	public class NewHandler extends AbstractFormHandler {
+
+		@Override
+		protected void execLoad() {
+			ISuTService service = BEANS.get(ISuTService.class);
+			SuTFormData formData = new SuTFormData();
+			exportFormData(formData);
+			formData = service.prepareCreate(formData);
+			importFormData(formData);
+
+			setEnabledPermission(new CreateSuTPermission());
+		}
+
+		@Override
+		protected void execStore() {
+			ISuTService service = BEANS.get(ISuTService.class);
+			SuTFormData formData = new SuTFormData();
+			exportFormData(formData);
+			service.create(formData);
+		}
+	}
+}
