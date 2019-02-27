1b502a612e6136a28145f7b5469d32c4d3ca4b73
Michael Theis <Michael.Theis@iosb.fraunhofer.de>
2019-02-27 13:47:32 +0100

    Report file access functions added

--- Command/src/main/java/nato/ivct/commander/SutPathsFiles.java
+++ Command/src/main/java/nato/ivct/commander/SutPathsFiles.java
@@ -46,60 +46,118 @@ public class SutPathsFiles {
 	}
 
 	/**
-	 * Get the path where the report file(s) are located
+	 * Get the path where the report file(s) of the requested SUT are located
 	 * @param sutName the desired SUT name
 	 * @return path where report file(s) are located
 	 */
-	public String getReportPath(final String sutName) {
+	public String getReportPath(final String sutId) {
 		String sutsHomePath = getSutsHomePath();
 		// Do not have any access to any SUTs
 		if (sutsHomePath == null) {
 			return null;
 		}
 
-		return sutsHomePath + "/" + sutName + "/Reports";
+		return sutsHomePath + "/" + sutId + "/Reports";
+	}
+
+	/**
+	 * Get the names of report files without path prefix for the requested SUT
+	 * 
+	 * @param sutId the ID of the desired SUT
+	 * @return a set of report file names
+	 */
+	public List<String> getSutReportFileNames(final String sutId) {
+		return getSutReportFileNames(sutId, false);
+	}
+
+	/**
+	 * Get the names of report files with/without path prefix depending on withPath parameter
+	 * 
+	 * @param sutId the ID of the desired SUT
+	 * @param withPath whether the path name is prefixed to report file name
+	 * @return a set of report file names
+	 */
+	public List<String> getSutReportFileNames(final String sutId, final boolean withPath) {
+		List<String> reportFileNames = new ArrayList<String>();
+		String path = getReportPath(sutId);
+		final File folder = new File(path);
+		if (folder.exists() == false || folder.isDirectory() == false) {
+			return reportFileNames;
+		}
+		reportFileNames = listLogFilesForFolder(folder, path, withPath);
+		return reportFileNames;
+	}
+
+
+	/**
+	 * Get report file names with/without path prefix depending on withPath parameter
+	 * 
+	 * @param folderName report file
+	 * @param path folder where to get the log files from
+	 * @param withPath whether the path name is prefixed to report file name
+	 * @return set of report file names
+	 */
+	private List<String> listReportFilesForFolder(final File folderName, final String path, final boolean withPath) {
+		List<String> reportFileNames = new ArrayList<String>();
+		if (folderName == null) {
+			return reportFileNames;
+		}
+	    for (final File fileEntry : folderName.listFiles()) {
+	        if (fileEntry.isFile()) {
+	        	String s = fileEntry.getName();
+	        	int len = s.length();
+	        	if (s.substring(len - 4, len).equals(".txt")) {
+	        		if (withPath) {
+	        			reportFileNames.add(path + "/" + fileEntry.getName());
+	        		} else {
+	        			reportFileNames.add(fileEntry.getName());
+	        		}
+	        	}
+	        }
+	    }
+		return reportFileNames;
 	}
 
 	/**
 	 * Get the path where the TcParam file(s) are located
-	 * @param sutName the desired SUT name
+	 * @param sutId the desired SUT ID
 	 * @return path where TcParam file(s) are located
 	 */
-	public String getTcParamPath(final String sutName, final String badgeName) {
+	public String getTcParamPath(final String sutId, final String badgeName) {
 		String sutsHomePath = getSutsHomePath();
 		// Do not have any access to any SUTs
 		if (sutsHomePath == null) {
 			return null;
 		}
 
-		return sutsHomePath + "/" + sutName + "/" + badgeName;
+		return sutsHomePath + "/" + sutId + "/" + badgeName;
 	}
 
 	/**
 	 * Get the names of the TcParam files available without path prefix.
 	 * Currently only one, may change in the future.
 	 * 
-	 * @param sutName the desired SUT name
+	 * @param sutId the desired SUT ID
 	 * @param badgeName the name of the badge under consideration
 	 * @return set of TcParam file names
 	 */
-	public List<String> getTcParamFileNames(final String sutName, final String badgeName) {
-		return getTcParamFileNames(sutName, badgeName, false);
+	public List<String> getTcParamFileNames(final String sutId, final String badgeName) {
+		return getTcParamFileNames(sutId, badgeName, false);
 	}
 
 	/**
-	 * Get the names of the TcParam files availablewith/without path prefix depending
+	 * Get the names of the TcParam files available with/without path prefix depending
 	 * on withPath parameter.
 	 * Currently only one, may change in the future
 	 *
-	 * @param sutName the desired SUT name
-	 * @param badgeName the name of the badge under consideration
+	 * @param sutId the desired SUT ID
+	 * @param badgeId the name of the badge under consideration
 	 * @return set of TcParam file names
 	 */
-	public List<String> getTcParamFileNames(final String sutName, final String badgeName, final boolean withPath) {
+	public List<String> getTcParamFileNames(final String sutId, final String badgeId, final boolean withPath) {
 		List<String> tcParamFileNames = new ArrayList<String>();
 
-		String folderName = getTcParamPath(sutName, badgeName);
+		String folderName = getTcParamPath(sutId, badgeId);
 		if (folderName == null) {
 			return tcParamFileNames;
 		}
@@ -127,25 +185,25 @@ public class SutPathsFiles {
 	/**
 	 * Get the names of log files without path prefix
 	 * 
-	 * @param sutName the name of the SUT
-	 * @param badgeName the name of the badge under consideration
+	 * @param sutId the ID of the SUT
+	 * @param badgeId the name of the badge under consideration
 	 * @return a set of log file names
 	 */
-	public List<String> getSutLogFileNames(final String sutName, final String badgeName) {
-		return getSutLogFileNames(sutName, badgeName, false);
+	public List<String> getSutLogFileNames(final String sutId, final String badgeId) {
+		return getSutLogFileNames(sutId, badgeId, false);
 	}
 
 	/**
 	 * Get the names of log files with/without path prefix depending on withPath parameter
 	 * 
-	 * @param sutName the name of the SUT
-	 * @param badgeName the name of the badge under consideration
+	 * @param sutId the ID of the SUT
+	 * @param badgeId the name of the badge under consideration
 	 * @param withPath whether the path name is prefixed to log file name
 	 * @return a set of log file names
 	 */
-	public List<String> getSutLogFileNames(final String sutName, final String badgeName, final boolean withPath) {
+	public List<String> getSutLogFileNames(final String sutId, final String badgeId, final boolean withPath) {
 		List<String> logFileNames = new ArrayList<String>();
-		String path = getSutLogPathName(sutName, badgeName);
+		String path = getSutLogPathName(sutId, badgeId);
 		final File folder = new File(path);
 		if (folder.exists() == false || folder.isDirectory() == false) {
 			return logFileNames;
@@ -157,22 +215,24 @@ public class SutPathsFiles {
 	/**
 	 * Get the path where the log files are located
 	 * 
-	 * @param sutName the name of the SUT
-	 * @param badgeName the name of the badge under consideration
+	 * @param sutId the ID of the SUT
+	 * @param badgeId the name of the badge under consideration
 	 * @return path where logfiles are located or null
 	 */
-	public String getSutLogPathName(final String sutName, final String badgeName) {
+	public String getSutLogPathName(final String sutId, final String badgeId) {
 		String sutsHome = getSutsHomePath();
 		if (sutsHome == null) {
 			return null;
 		}
-		return sutsHome + "/" + sutName + "/" + badgeName + "/Logs";
+		return sutsHome + "/" + sutId + "/" + badgeId + "/Logs";
 	}
 
 	/**
 	 * Get log file names with/without path prefix depending on withPath parameter
 	 * 
-	 * @param logfile folderName
+	 * @param folderName log file
+	 * @param path folder where to get the log files from
+	 * @param withPath whether the path name is prefixed to log file name
 	 * @return set of logfile names
 	 */
 	private List<String> listLogFilesForFolder(final File folderName, final String path, final boolean withPath) {
