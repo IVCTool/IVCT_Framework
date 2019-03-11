6aa8d9cecfc1b0734d8221c21b73cfdc2b684ca0
Michael Theis <Michael.Theis@iosb.fraunhofer.de>
2019-03-11 14:26:25 +0100

    test report form added

---
+++ GUI/nato.ivct.gui.client/src/test/java/nato/ivct/gui/client/sut/TestReportFormTest.java
@@ -0,0 +1,15 @@
+package nato.ivct.gui.client.sut;
+
+import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
+import org.eclipse.scout.rt.testing.client.runner.ClientTestRunner;
+import org.eclipse.scout.rt.testing.client.runner.RunWithClientSession;
+import org.eclipse.scout.rt.testing.platform.runner.RunWithSubject;
+import org.junit.runner.RunWith;
+
+@RunWithSubject("anonymous")
+@RunWith(ClientTestRunner.class)
+@RunWithClientSession(TestEnvironmentClientSession.class)
+public class TestReportFormTest {
+
+	// TODO [the] add test cases
+}
