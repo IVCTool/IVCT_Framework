719a7b908068654e74fe4e9cbdf8ce4f2f553296
ducana <johannes.mulder@iosb.fraunhofer.de>
2019-04-17 09:04:34 +0200

    Fix problem with empty badge array.

--- Command/src/main/java/nato/ivct/commander/CmdUpdateSUT.java
+++ Command/src/main/java/nato/ivct/commander/CmdUpdateSUT.java
@@ -167,6 +167,8 @@ public class CmdUpdateSUT {
 						} else {
 							return true;
 						}
+					} else {
+						badgeArray = new JSONArray();
 					}
 					boolean dataFound  = false;
 					for (int i = 0; i < badgeArray.size(); i++) {
