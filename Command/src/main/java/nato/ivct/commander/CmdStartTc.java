0eb8915102f0304002435ba605f70916b82cc08d
Reinhard Herzog <reinhard.herzo@iosb.fraunhofer.de>
2019-08-26 13:56:17 +0200

    startTc with sutFederateName

--- Command/src/main/java/nato/ivct/commander/CmdStartTc.java
+++ Command/src/main/java/nato/ivct/commander/CmdStartTc.java
@@ -20,57 +20,51 @@ import org.json.simple.parser.ParseException;
 import org.slf4j.LoggerFactory;
 
 public class CmdStartTc implements Command {
-    
-    public static final String COMMAND_ID          = "commandType";
-    public static final String COMMAND             = "startTestCase";
-    public static final String SEQ                 = "sequence";
-    public static final String SUT_NAME            = "sutName";
-    public static final String SUT_DIR             = "sutDir";
-    public static final String BADGE               = "badge";
-    public static final String TC_ID               = "testCaseId";
-    public static final String TC_PARAM            = "tcParam";
-    public static final String SETTINGS_DESIGNATOR = "settingsDesignator";
-    public static final String FEDERATION          = "federationName";
-    public static final String FEDERATE            = "sutFederateName";
-            
+
+	public static final String COMMAND_ID = "commandType";
+	public static final String COMMAND = "startTestCase";
+	public static final String SEQ = "sequence";
+	public static final String SUT_NAME = "sutName";
+	public static final String SUT_DIR = "sutDir";
+	public static final String BADGE = "badge";
+	public static final String TC_ID = "testCaseId";
+	public static final String TC_PARAM = "tcParam";
+	public static final String SETTINGS_DESIGNATOR = "settingsDesignator";
+	public static final String FEDERATION = "federationName";
+	public static final String FEDERATE = "sutFederateName";
+
 	// private MessageProducer producer;
 	private String sut;
 	private String badge;
-    private String tc;
-    private String settingsDesignator;
-    private String federationName;
+	private String tc;
+	private String settingsDesignator;
+	private String federationName;
+	private String sutFederateName;
 
 	public static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(CmdStartTc.class);
 
-	public CmdStartTc(String _sut, String _badge, String _tc, String _settingsDesignator, String _federationName) {
+	public CmdStartTc(String _sut, String _badge, String _tc, String _settingsDesignator, String _federationName,
+			String _sutFederateName) {
 		sut = _sut;
 		tc = _tc;
 		badge = _badge;
 		settingsDesignator = _settingsDesignator;
 		federationName = _federationName;
+		sutFederateName = _sutFederateName;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	/*
-	 * The Structure of start test case command message looks like the
-	 * following:
+	 * The Structure of start test case command message looks like the following:
 	 *
-	 * { 
-	 *   "commandType":"startTestCase",
-	 *   "sequence":"0",
-	 *   "testScheduleName":"HelloWorld",
-	 *   "sutName":"hw_iosb",
-	 *   "sutDir":"C:/projects/IVCT_Runtime/IVCTsut/hw_iosb",
-	 *   "badge":"C:/projects/IVCT_Runtime/Badges/HelloWorld-1.0.0",
-	 *   "testCaseId":"TC0002",
-	 *   "settingsDesignator":"crcAddress=localhost:8989",
-	 *   "federationName":"TheWorld",
-	 *   "tcParam":{
-     *     "sutFederateName":"A" 
-     *     "testDuration":"120" 
-	 *     }
-	 *   }
+	 * { "commandType":"startTestCase", "sequence":"0",
+	 * "testScheduleName":"HelloWorld", "sutName":"hw_iosb",
+	 * "sutDir":"C:/projects/IVCT_Runtime/IVCTsut/hw_iosb",
+	 * "badge":"C:/projects/IVCT_Runtime/Badges/HelloWorld-1.0.0",
+	 * "testCaseId":"TC0002", "settingsDesignator":"crcAddress=localhost:8989",
+	 * "federationName":"TheWorld", "tcParam":{ "sutFederateName":"A"
+	 * "testDuration":"120" } }
 	 *
 	 * @see nato.ivct.commander.Command#execute()
 	 */
@@ -86,15 +80,16 @@ public class CmdStartTc implements Command {
 			startCmd.put(SUT_NAME, sut);
 			startCmd.put(SUT_DIR, sutHome + '/' + sut);
 			startCmd.put(BADGE, badge);
-            startCmd.put(TC_ID, tc);
-            startCmd.put(SETTINGS_DESIGNATOR, settingsDesignator);
-            startCmd.put(FEDERATION, federationName);
-			
+			startCmd.put(TC_ID, tc);
+			startCmd.put(SETTINGS_DESIGNATOR, settingsDesignator);
+			startCmd.put(FEDERATION, federationName);
+			startCmd.put(FEDERATE, sutFederateName);
+
 			String paramFileContentString = Factory.readWholeFile(paramFileName);
 			if (paramFileContentString != null) {
-    			String tmpString = Factory.replaceMacro(paramFileContentString);
-    			JSONObject jsonParam = (JSONObject) parser.parse(tmpString);
-    			startCmd.put(TC_PARAM, jsonParam);
+				String tmpString = Factory.replaceMacro(paramFileContentString);
+				JSONObject jsonParam = (JSONObject) parser.parse(tmpString);
+				startCmd.put(TC_PARAM, jsonParam);
 			} else {
 				LOGGER.error("File not found: " + paramFileName);
 				return;
@@ -134,20 +129,20 @@ public class CmdStartTc implements Command {
 		this.badge = _badge;
 	}
 
-    public String getSettingsDesignator() {
-        return settingsDesignator;
-    }
+	public String getSettingsDesignator() {
+		return settingsDesignator;
+	}
 
-    public void setSettingsDesignator(String settingsDesignator) {
-        this.settingsDesignator = settingsDesignator;
-    }
+	public void setSettingsDesignator(String settingsDesignator) {
+		this.settingsDesignator = settingsDesignator;
+	}
 
-    public String getFederationName() {
-        return federationName;
-    }
+	public String getFederationName() {
+		return federationName;
+	}
 
-    public void setFederationName(String federationName) {
-        this.federationName = federationName;
-    }
+	public void setFederationName(String federationName) {
+		this.federationName = federationName;
+	}
 
 }
