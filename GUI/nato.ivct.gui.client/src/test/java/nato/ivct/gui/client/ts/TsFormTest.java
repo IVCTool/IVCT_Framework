b7ede51d94307d39b87d50b225baadaf2fdbcbf7
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2020-01-28 15:10:32 +0100

    quick fix for junit tests

--- GUI/nato.ivct.gui.client/src/test/java/nato/ivct/gui/client/ts/TsFormTest.java
+++ GUI/nato.ivct.gui.client/src/test/java/nato/ivct/gui/client/ts/TsFormTest.java
@@ -1,9 +1,12 @@
 package nato.ivct.gui.client.ts;
 
+import static org.junit.Assert.*;
+
 import org.eclipse.scout.rt.client.testenvironment.TestEnvironmentClientSession;
 import org.eclipse.scout.rt.testing.client.runner.ClientTestRunner;
 import org.eclipse.scout.rt.testing.client.runner.RunWithClientSession;
 import org.eclipse.scout.rt.testing.platform.runner.RunWithSubject;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWithSubject("anonymous")
@@ -12,4 +15,8 @@ import org.junit.runner.RunWith;
 public class TsFormTest {
 
 	// TODO [the] add test cases
+	@Test
+	public void testName() throws Exception {
+		
+	}
 }
