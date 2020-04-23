8e59b76c265cd58af5ad11bf7555c9f8132e28dc
Felix Schöppenthau <felix.schoeppenthau@iosb.fraunhofer.de>
2020-04-23 11:48:16 +0200

    Create JSON report source for PDF report

--- GUI/nato.ivct.gui.server/src/main/java/nato/ivct/gui/server/sut/SuTService.java
+++ GUI/nato.ivct.gui.server/src/main/java/nato/ivct/gui/server/sut/SuTService.java
@@ -7,13 +7,17 @@ import java.nio.file.InvalidPathException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Stream;
@@ -23,16 +27,34 @@ import org.eclipse.scout.rt.platform.exception.VetoException;
 import org.eclipse.scout.rt.platform.job.IFuture;
 import org.eclipse.scout.rt.platform.resource.BinaryResource;
 import org.eclipse.scout.rt.platform.text.TEXTS;
+import org.eclipse.scout.rt.shared.data.tile.TileColorScheme;
 import org.eclipse.scout.rt.shared.services.common.security.ACCESS;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import nato.ivct.commander.BadgeDescription;
 import nato.ivct.commander.CmdListSuT;
 import nato.ivct.commander.CmdUpdateSUT;
 import nato.ivct.commander.Factory;
 import nato.ivct.commander.SutDescription;
+import nato.ivct.commander.CmdListTestSuites.TestSuiteDescription;
 import nato.ivct.gui.server.ServerSession;
+import nato.ivct.gui.server.ServerSession.SutTcResultDescription;
 import nato.ivct.gui.server.cb.CbService;
+import nato.ivct.gui.server.ts.TsService;
+import nato.ivct.gui.shared.cb.ICbService;
 import nato.ivct.gui.shared.cb.ITsService;
 import nato.ivct.gui.shared.sut.CreateSuTPermission;
 import nato.ivct.gui.shared.sut.ISuTService;
@@ -41,6 +63,8 @@ import nato.ivct.gui.shared.sut.ReadSuTPermission;
 import nato.ivct.gui.shared.sut.SuTEditFormData;
 import nato.ivct.gui.shared.sut.SuTFormData;
 import nato.ivct.gui.shared.sut.SuTFormData.SutCapabilityStatusTable.SutCapabilityStatusTableRowData;
+import nato.ivct.gui.shared.ts.TsFormData;
+import nato.ivct.gui.shared.ts.TsFormData.TcTable.TcTableRowData;
 import nato.ivct.gui.shared.sut.UpdateSuTPermission;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
@@ -133,37 +157,205 @@ public class SuTService implements ISuTService {
 
     @Override
     public String createTestreport(final String sutId){
+        //TODO
         Path reportFolder = Paths.get("C:\\Entwicklung\\IVCT\\IVCT_Runtime\\IVCTsut\\"+sutId+"\\Reports\\");
         Path templateFolder = Paths.get("C:\\Entwicklung\\IVCT\\IVCT_Runtime\\IVCTsut\\"+sutId+"\\Reports\\Template\\");
         
-        if (createReportJsonFile(sutId, reportFolder.toString() +"\\" + "Result.json").isEmpty()) {
+        if (createReportJsonFile(sutId, reportFolder.toString() +"\\" + "Results.json").isEmpty()) {
             return null;
-        }
-        
+        }  
         
         return createPDFTestreport(templateFolder, reportFolder);
     }
     
     private final String createReportJsonFile(final String sutId, final String resultFile) {
-        String reportFile = "";
+
+        final JsonObject jReport = new JsonObject();
         
+        //Result.json to JSON Object       
+        final Optional<JsonObject> jResults = readJsonResultFile(sutId, resultFile);
+        
+        if (!jResults.isPresent()) {
+            return "";
+        }
+        
+        //Create JSON Report Structure
         /*
-         * Result.json to JSON Object: Einlesen von Result.json
-         * SuT Informationen einlesen: Aus SuTForm einlesen
-         * Badge Informationen für SuT einlesen: Aus SuTForm einlesen
-         * Badge Verdict einlesen: Aus SuTForm einlesen
-         * Alle relevanten TS von einer Badge: Aus SuTCbForm einlesen
-         * Alle relevanten TC von einer TS: Aus SuTCbForm einlesen
-         * Alle TcResults von einem TC: Aus Results.json einlesen
-         * Finaler Verdict für SuT: Analog wie für Badge Verdict
-         * 
+         * 1.SuT
+         * 2.VerdictSummary
+         * 3.Badges
+         * 3.1. TestSuites
+         * 3.1.1. TestCases
+         * 3.1.1.1. TcResults
          */
+           
+        //SuT section
+        transformSuTDesc(jResults.get(), jReport);
         
+        //VerdictSummary section
+        transformVerdictSummary(jResults.get(), jReport);
         
+        //Badges section
+        transformBadges(jResults.get(), jReport, sutId);
+        
+        //Fill with calculated SuT Verdict
+        addSutVerdict(jReport);
+ 
+        return reportJSONFile(jReport);
+    }
+ 
+    private String reportJSONFile(JsonElement jReport) {           
+        String reportFile = "";  
+        
+        try {
+            //TODO
+            reportFile = "C:\\Entwicklung\\IVCT\\IVCT_Runtime\\IVCTsut\\hw_iosb\\Reports\\Report.json";
+            LOG.debug("store report json object to file {}", reportFile);
+            Files.write(Paths.get(reportFile), jReport.toString().getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+        }
+        catch (final InvalidPathException e) {
+            LOG.error("invalid path for report json file", reportFile);
+        }
+        catch (final IOException e) {
+            LOG.error("could not write report json object to file {}", reportFile);
+        }
         return reportFile;
     }
     
+    private void transformSuTDesc(final JsonObject jResults, final JsonObject jReport) {
+        final String SUT_KW = "SuT";
+        
+        JsonObject sutSection = (JsonObject) jResults.get(SUT_KW);
+        jReport.add(SUT_KW, sutSection);  
+    }
     
+    private void transformVerdictSummary(final JsonObject jResults, final JsonObject jReport) {
+        final String VERDICTSUMMARY_KW = "VerdictSummary";
+        final String SUTVERDICT_KW = "SutVerdict";
+        
+        JsonObject verdictSection = (JsonObject) jResults.get(VERDICTSUMMARY_KW);
+        
+        jReport.add(VERDICTSUMMARY_KW, verdictSection);
+    }
+    
+    private void transformBadges(final JsonObject jResults, final JsonObject jReport, final String sutId) {
+        final String BADGES_KW = "Badges";
+        final String BADGEID_KW = "BadgeId";
+        final String BADGENAME_KW = "BadgeName";
+        final String BADGEVERDICT_KW = "BadgeVerdict";
+        
+        //SuT information
+        final SutDescription sutDesc = BEANS.get(SuTService.class).getSutDescription(sutId);
+        
+        //Badge information
+        Set<String> badges = sutDesc.badges;
+        
+        JsonArray badgeArray = new JsonArray();
+        jReport.add(BADGES_KW, badgeArray);
+        
+        badges.stream().sorted().forEachOrdered(badgeId ->{
+            BadgeDescription badgeDesc = BEANS.get(CbService.class).getBadgeDescription(badgeId);
+            JsonObject badgeObj = new JsonObject();
+            
+            badgeObj.addProperty(BADGEID_KW, badgeDesc.ID);
+            badgeObj.addProperty(BADGENAME_KW, badgeDesc.name);
+            badgeObj.addProperty(BADGEVERDICT_KW, getBadgeConformanceStatus(sutId, badgeId));
+  
+            badgeArray.add(badgeObj);
+            
+            //TestSuite section
+            transformTestSuites(jResults, badgeObj, badgeId);
+        }); 
+    }
+    
+    private void transformTestSuites(final JsonObject jResults, final JsonObject badgeObj, final String badgeId) {
+        final String TESTSUITES_KW = "TestSuites";
+        final String TSID_KW = "TsId";
+        final String TSNAME_KW = "TsName";
+        
+        //TestSuite information
+        final Set<String> tsList = BEANS.get(ITsService.class).getTsForIr(BEANS.get(ICbService.class).getIrForCb(badgeId));
+        
+        JsonArray tsArray = new JsonArray();
+        badgeObj.add(TESTSUITES_KW, tsArray);
+        
+        tsList.stream().sorted().forEachOrdered(tsId ->{   
+            TestSuiteDescription tsDesc = BEANS.get(TsService.class).getTsDescription(tsId); 
+            JsonObject tsObj = new JsonObject();
+            
+            tsObj.addProperty(TSID_KW, tsDesc.id);
+            tsObj.addProperty(TSNAME_KW, tsDesc.name);
+  
+            tsArray.add(tsObj);
+            
+            //TestCase section
+            transformTestCases(jResults, tsObj, badgeId, tsId);
+        });       
+    }
+    
+    private void transformTestCases(final JsonObject jResults, final JsonObject tsObj, final String bdId, final String tsId) {
+        final String TESTCASES_KW = "TestCases";
+        final String TCID_KW = "TcId";
+        final String TCNAME_KW = "TcName";
+        final String TCRESULTS_KW = "TcResults";
+        
+        //TestCase information
+        final Map<String, HashSet<String>> tcList = BEANS.get(ITsService.class).getTcListForBadge(bdId);
+        
+        JsonArray tcArray = new JsonArray();
+        tsObj.add(TESTCASES_KW, tcArray);
+        
+        tcList.getOrDefault(tsId, new HashSet<String>()).stream().sorted().forEachOrdered(tcId -> {
+            
+            String tcName = Stream.of(tcId.split(Pattern.quote("."))).reduce((a, b) -> b).get();
+
+            JsonObject tcObj = new JsonObject();
+            
+            tcObj.addProperty(TCID_KW, tcId);
+            tcObj.addProperty(TCNAME_KW, tcName);
+
+            tcArray.add(tcObj);
+            
+            JsonObject results = (JsonObject) jResults.get(TCRESULTS_KW);
+            JsonObject tsSection = (JsonObject) results.get(tsId);
+            JsonArray tcSection = (JsonArray) tsSection.get(tcId);
+            
+            //TcResult section
+            transformTcResults(tcSection, tcObj);
+        });  
+    }
+
+
+    private void transformTcResults(JsonArray tcSection, JsonObject tcObj) {
+        final String TCRESULTS_KW = "TcResults";
+
+        //TcResult information
+        JsonArray tcResultArray = new JsonArray();
+        tcObj.add(TCRESULTS_KW, tcResultArray);
+        
+        tcSection.forEach(result ->{
+            tcResultArray.add(result);
+        });
+    }
+    
+    private void addSutVerdict(final JsonObject jReport) {
+        final String VERDICTSUMMARY_KW = "VerdictSummary";
+        final String SUTVERDICT_KW = "SutVerdict";
+        
+        String sutConformanceStatus = getSutConformanceStatus(jReport);
+        jReport.getAsJsonObject(VERDICTSUMMARY_KW).addProperty(SUTVERDICT_KW, sutConformanceStatus);
+        
+    }
+    
+    private Optional<JsonObject> readJsonResultFile(final String sutId, final String resultFile) {
+        try {
+            return Optional.ofNullable(JsonParser.parseReader((Files.newBufferedReader(Paths.get(resultFile)))).getAsJsonObject());
+        }
+        catch (IOException | JsonIOException | JsonSyntaxException exc) {
+            LOG.error("Result file does not exist", resultFile);
+            return Optional.empty();
+        }
+    }   
     
     private String createPDFTestreport(final Path templateFolder, final Path reportFolder) {
 
@@ -269,6 +461,18 @@ public class SuTService implements ISuTService {
         return fd;
     }
 
+    private String getSutConformanceStatus(final JsonObject jReport) {
+        final String BADGES_KW = "Badges";
+        final String BADGEVERDICT_KW = "BadgeVerdict";
+        
+        String sutVerdict = "PASSED";
+        for (JsonElement badge : jReport.getAsJsonArray(BADGES_KW)){
+            String bdVerdict = badge.getAsJsonObject().get(BADGEVERDICT_KW).getAsString();
+            sutVerdict = evalVerdicts(sutVerdict, bdVerdict);
+        }
+        
+        return sutVerdict; 
+    }
 
     private String getBadgeConformanceStatus(final String sutId, final String badgeId) {
         String bdVerdict = "PASSED";
@@ -290,13 +494,13 @@ public class SuTService implements ISuTService {
     }
 
 
-    private String evalVerdicts(final String bdVerdict, final String tcVerdict) {
+    private String evalVerdicts(final String summaryVerdict, final String individualVerdict) {
         //PASSED < UNKNOWN < INCONCLUSIVE < FAILED
-        String verdict = bdVerdict;
-        if (!"PASSED".equalsIgnoreCase(tcVerdict)) {
-            if ("FAILED".equalsIgnoreCase(tcVerdict) || "FAILED".equalsIgnoreCase(bdVerdict))
+        String verdict = summaryVerdict;
+        if (!"PASSED".equalsIgnoreCase(individualVerdict)) {
+            if ("FAILED".equalsIgnoreCase(individualVerdict) || "FAILED".equalsIgnoreCase(summaryVerdict))
                 verdict = "FAILED";
-            else if ("INCONCLUSIVE".equalsIgnoreCase(tcVerdict) || "INCONCLUSIVE".equalsIgnoreCase(bdVerdict))
+            else if ("INCONCLUSIVE".equalsIgnoreCase(individualVerdict) || "INCONCLUSIVE".equalsIgnoreCase(summaryVerdict))
                 verdict = "INCONCLUSIVE";
             else
                 verdict = "UNKNOWN";
