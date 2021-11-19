48694d13b2e6418a625af389e076657244fd7bb9
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2021-11-19 10:30:29 +0100

    cleanup

--- Command/src/test/java/nato/ivct/commander/InitializationTest.java
+++ Command/src/test/java/nato/ivct/commander/InitializationTest.java
@@ -15,34 +15,9 @@ limitations under the License. */
 package nato.ivct.commander;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.apache.activemq.broker.BrokerService;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class InitializationTest {
-	private static BrokerService broker = new BrokerService();
-
-	@BeforeAll
-	public static void startBroker() throws Exception {
-		// configure the broker
-		broker.addConnector("tcp://localhost:61616"); 
-		broker.setPersistent(false);
-
-		broker.start();
-        Factory.initialize();
-	}
-
-	@AfterAll
-	public static void stopBroker() throws Exception {
-		try {
-			broker.stop();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+public class InitializationTest  {
 
     @Test
     public void testReadVersion() {
