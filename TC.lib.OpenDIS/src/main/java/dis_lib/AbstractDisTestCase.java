2bf75b2606273f94e13011d970e233b86e61228d
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2022-02-22 08:33:57 +0100

    add facade method

--- TC.lib.OpenDIS/src/main/java/dis_lib/AbstractDisTestCase.java
+++ TC.lib.OpenDIS/src/main/java/dis_lib/AbstractDisTestCase.java
@@ -28,4 +28,20 @@ public abstract class AbstractDisTestCase extends AbstractTestCaseIf {
         ivct_Verdict.verdict = IVCT_Verdict.Verdict.PASSED;
         return ivct_Verdict;
     }
+
+     /**
+     * Send a text message to the IVCT operator and wait for confirmation
+     * This method overwrites the inherited method and remaps the exception, 
+     * in order to hide the internal API.
+     * 
+     * @param text String zu be sent to the ivct operator
+     * @throws TcInconclusive Exception thrown if operator cancels confirmation
+     */
+    public void sendOperatorRequest(String text) throws TcInconclusive {
+        try {
+            super.sendOperatorRequest(text);
+        } catch (TcInconclusiveIf e) {
+            throw new TcInconclusive(e.getMessage());
+        }
+    }    
 }
