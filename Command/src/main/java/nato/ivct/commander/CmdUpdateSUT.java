cc01f6833f5e550076e74d44c4fa5db656ee8176
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2023-03-01 15:52:10 +0100

    avoid re-initialization of sut list

--- Command/src/main/java/nato/ivct/commander/CmdUpdateSUT.java
+++ Command/src/main/java/nato/ivct/commander/CmdUpdateSUT.java
@@ -23,7 +23,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.channels.FileLock;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
